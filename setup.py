1b9b6cf5e575ae5604d27bc33b29aa70b951f040
Anup Pokhrel <virtualanup@gmail.com>
2017-01-10 22:59:00 -0500

    Update requirements in setup.py , added setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from distutils.core import setup
 setup(
-    name='hal-assistant',
+    name='hal',
     packages=['hal'],
     version='0.1',
     description='Command Line Assistant',
     author='Anup Pokhrel',
     author_email='virtualanup@gmail.com',
     url='https://github.com/virtualanup/hal',
-    download_url='https://github.com/virtualanup/hal/tarball',
+    download_url='https://github.com/virtualanup/hal/archive/0.1.tar.gz',
     keywords=['assistant', 'hal'],
     classifiers=[],
+    install_requires=[
+          'six==1.10.0',
+          'simpleeval==0.9.1',
+          'pytz==2016.10'
+      ],
 )
