984469911a1d369141806d3f923b826fe3fd5a33
Anup Pokhrel <virtualanup@gmail.com>
2017-01-28 00:36:32 -0500

    Added few functions support in calc library

--- hal/lib/calc.py
+++ hal/lib/calc.py
@@ -2,6 +2,7 @@
 
 import re
 import random
+import math
 
 from simpleeval import simple_eval
 from hal.library import HalLibrary
@@ -15,9 +16,29 @@ class CalcLib(HalLibrary):
     name = "Calculator"
     keywords = ["calc", "calculate", "eval"]
 
+    functions = {
+        "sin": math.sin,
+        "cos": math.cos,
+        "tan": math.tan,
+        "ceil": math.ceil,
+        "floor": math.floor,
+        "factorial": math.factorial,
+        "log": math.log,
+        "log10": math.log10,
+        "sqrt": math.sqrt,
+        "square": lambda x: x*x,
+        "sqrt": lambda x: x**0.5,
+
+    }
+
     calcregex = re.compile(
         "(value\s+of\s+)?(.*)", re.IGNORECASE
     )
+
+    function_regex = re.compile(
+        "([a-zA-Z0-9]+)\s+of\s+(.*)", re.IGNORECASE
+    )
+
 
     calc_command_regex = re.compile(
         "(calc|eval|calculate|evaluate)\s+(the\s+)?", re.IGNORECASE
@@ -32,17 +53,42 @@ class CalcLib(HalLibrary):
                 # nothing to do
                 return
 
+        calc_command = self.command
+
         if self.match_and_reduce(self.calcregex):
             expression = self.last_matched.groups()[-1].strip()
             if expression:
                 try:
                     # Try to parse the expression here.
-                    result = simple_eval(expression)
+                    result = simple_eval(
+                        expression,
+                        functions=self.functions
+                    )
                     self.status = self.SUCCESS
                     self.result = result
+                    return
                 except:
                     # Fail silently
                     pass
+        # restore the calc command to interpret it in other way
+        self.command = calc_command
+        if self.match_and_reduce(self.function_regex):
+            function_name = self.last_matched.groups()[-2].strip()
+            expression = self.last_matched.groups()[-1].strip()
+
+            created_expression = "{}({})".format(function_name.lower(), expression)
+            try:
+                # Try to parse the expression here.
+                result = simple_eval(
+                    created_expression,
+                    functions=self.functions
+                )
+                self.status = self.SUCCESS
+                self.result = result
+                return
+            except:
+                # Fail silently
+                pass
 
     def process(self):
         self.add_response("Result : " + str(self.result))
