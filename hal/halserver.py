f2e1a409c30e35a38d1ab61fbaa040222f17e9a4
Anup Pokhrel <virtualanup@gmail.com>
2017-06-18 14:48:43 -0400

    Seperate function for server entry

--- hal/halserver.py
+++ hal/halserver.py
@@ -51,7 +51,7 @@ class HalDaemon(Daemon):
                 return False
 
 
-if __name__ == "__main__":
+def main():
     daemon = HalDaemon('/tmp/daemon-example.pid')
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
@@ -64,9 +64,11 @@ if __name__ == "__main__":
             daemon.test()
         else:
             print("Unknown command")
-            sys.exit(2)
-        sys.exit(0)
+            return 2
+        return 0
     else:
         print("usage: %s start|stop|restart" % sys.argv[0])
-        sys.exit(2)
+        return 2
 
+if __name__ == "__main__":
+    sys.exit(main())
